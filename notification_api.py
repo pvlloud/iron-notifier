f8afd2642c4c487e1a5dd612d27c23406f651be1
pvlsshk <pvlsshk@gmail.com>
2017-08-29 23:23:44 +0300

    Add mock API for testing

--- notification_api.py
+++ notification_api.py
@@ -1,5 +1,7 @@
 import os
 
+import random
+
 from flask import Flask
 from emr_status import emr_client
 from emr_status import get_status
@@ -15,3 +17,10 @@ def status():
     emr = emr_client(aws_key, aws_secret)
     status_code = get_status(emr)
     return status_code.value
+
+
+@application.route('/mock')
+def mock():
+    test_arr = [b'0', b'1', b'2']
+    value = random.randint(0, 2)
+    return test_arr[value]
